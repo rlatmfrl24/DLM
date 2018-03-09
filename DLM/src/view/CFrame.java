96e647eef3ba303622ef21e99e14032cc9846cf1
rlatmfrl24 <rlatmfrl24@naver.com>
2018-03-09 11:35:55 +0900

    Categorizer 1차 릴리즈

--- DLM/src/view/CFrame.java
+++ DLM/src/view/CFrame.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.util.Observable;
 import java.util.Observer;
 
+import org.apache.commons.io.FileExistsException;
+import org.apache.commons.io.FileUtils;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
@@ -13,6 +15,7 @@ import org.eclipse.swt.widgets.DirectoryDialog;
 
 import swing2swt.layout.BorderLayout;
 import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.layout.GridLayout;
@@ -26,10 +29,13 @@ import main.ConfigLoader;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import util.AutoCategorizer;
+import org.eclipse.swt.layout.RowLayout;
 
 public class CFrame implements Observer {
 	private static Text text_path;
 	private static ConfigLoader config;
+	private static Tree tree_transform;
+	private static AutoCategorizer ac = new AutoCategorizer();
 	
 	public CFrame(ConfigLoader c) {
 		config = c;
@@ -53,47 +59,82 @@ public class CFrame implements Observer {
 		composite_path.setLayoutData(BorderLayout.NORTH);
 		composite_path.setLayout(new FillLayout(SWT.HORIZONTAL));
 		
+		Composite composite_center = new Composite(main_composite, SWT.NONE);
+		composite_center.setLayout(new GridLayout(3, false));
+		
+		Tree tree_origin = new Tree(composite_center, SWT.BORDER);
+		tree_origin.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		
+		Composite composite = new Composite(composite_center, SWT.NONE);
+		RowLayout rl_composite = new RowLayout(SWT.HORIZONTAL);
+		rl_composite.justify = true;
+		composite.setLayout(rl_composite);
+		
+		Button button = new Button(composite, SWT.NONE);
+		button.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				for(TreeItem item : tree_origin.getItems()) {
+					String transform_path = ac.GetCategorizedName((File)item.getData(item.getText()));
+					tree_transform = addPath(transform_path, tree_transform);
+				}
+			}
+		});
+		button.setText("→");
+		
+				tree_transform = new Tree(composite_center, SWT.BORDER);
+				tree_transform.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1));
+		
 		Group grpTargetPath = new Group(composite_path, SWT.NONE);
 		grpTargetPath.setText("Target Path");
 		grpTargetPath.setLayout(new GridLayout(2, false));
 		
 		text_path = new Text(grpTargetPath, SWT.BORDER | SWT.READ_ONLY);
 		text_path.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		text_path.setText(config.GetCurrentPath());
+		text_path.setText("Please Select Folder..");
 		
-		SashForm sashForm = new SashForm(main_composite, SWT.NONE);
-		sashForm.setLayoutData(BorderLayout.CENTER);
+		Composite composite_command = new Composite(main_composite, SWT.NONE);
+		composite_command.setLayoutData(BorderLayout.SOUTH);
+		composite_command.setLayout(new GridLayout(1, false));
 		
-		Tree tree_origin = new Tree(sashForm, SWT.BORDER);
-		
-		Composite composite = new Composite(sashForm, SWT.NONE);
-		composite.setLayout(new GridLayout(1, true));
-
-		Tree tree_transform = new Tree(sashForm, SWT.BORDER);
-		sashForm.setWeights(new int[] {141, 27, 141});
-		
-		Button button = new Button(composite, SWT.NONE);
-		button.addSelectionListener(new SelectionAdapter() {
+		Button btnApply = new Button(composite_command, SWT.NONE);
+		btnApply.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				AutoCategorizer ac = new AutoCategorizer();
-				for(TreeItem item : tree_origin.getItems()) {
-					String[] transform_path = ac.GetCategorizedName((File)item.getData(item.getText())).split("/");
-					for(String s : transform_path) {
-						System.out.println(s);
+				try {
+					if(tree_transform.getItems().length==0) {
+						//MessageBox
+						MessageBox msg = new MessageBox(shell);
+						msg.setText("Alert");
+						msg.setMessage("Check transform preview first..");
+						msg.open();
+					}else {
+						for (TreeItem item : tree_origin.getItems()) {
+							String transform_path = ac.GetCategorizedName((File) item.getData(item.getText()));
+							try {
+								FileUtils.moveFile((File) item.getData(item.getText()), new File(text_path.getText()+"/"+transform_path));
+							}catch(FileExistsException fee){
+								File duplicated_file = (File) item.getData(item.getText());
+								MessageBox msg = new MessageBox(shell);
+								msg.setText("Alert");
+								msg.setMessage(duplicated_file.getName()+"\n\nThis File is Dulicated, Check This File");
+								msg.open();
+							}
+						}
+						MessageBox msg = new MessageBox(shell);
+						msg.setText("Alert");
+						msg.setMessage("Transform Done!");
+						msg.open();
+						tree_origin.removeAll();
+						tree_transform.removeAll();
 					}
+				}catch (Exception ex) {
+					ex.printStackTrace();
 				}
+
 			}
 		});
-		button.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, true, 1, 1));
-		button.setText("→");
-		
-		Composite composite_1 = new Composite(main_composite, SWT.NONE);
-		composite_1.setLayoutData(BorderLayout.SOUTH);
-		composite_1.setLayout(new GridLayout(1, false));
-		
-		Button btnDone = new Button(composite_1, SWT.NONE);
-		btnDone.setText("Done");
+		btnApply.setText("Apply");
 		
 		Button btnBrowse = new Button(grpTargetPath, SWT.NONE);
 		btnBrowse.addSelectionListener(new SelectionAdapter() {
@@ -130,6 +171,45 @@ public class CFrame implements Observer {
 			}
 		}
 	}
+	
+public Tree addPath(String s, Tree root) {	
+		
+		TreeItem parent = null;
+		for(TreeItem item : root.getItems()) {
+			if(item.getText().equals(s.split("/")[0])) {
+				s = s.substring(s.indexOf('/')+1, s.length());
+				parent = item;
+			}
+		}
+		if(parent == null) {
+			TreeItem item = new TreeItem(root, 0);
+			item.setText(s.split("/")[0]);
+			s = s.substring(s.indexOf('/')+1, s.length());
+			parent = item;
+		}
+		
+		while(s.indexOf('/')!=-1) {
+
+			TreeItem subitem = null;
+			for(TreeItem item : parent.getItems()) {
+				if(item.getText().equals(s.split("/")[0])) {
+					s = s.substring(s.indexOf('/')+1, s.length());
+					subitem = item;
+					parent = subitem;
+				}
+			}
+			if(subitem == null) {
+				subitem = new TreeItem(parent, 0);
+				subitem.setText(s.split("/")[0]);
+				s = s.substring(s.indexOf('/')+1, s.length());
+				parent = subitem;
+			}
+		}
+		TreeItem subitem = new TreeItem(parent, 0);
+		subitem.setText(s);
+		return root;
+	}
+	
 	@Override
 	public void update(Observable o, Object arg) {
 		// TODO Auto-generated method stub
