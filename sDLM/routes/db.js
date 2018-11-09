e458d27ec7f1fdd9482441e95848c08615672019
freight9 <kimseulki@freight9.com>
2018-11-09 09:24:31 +0900

    서버 접속 정보 변경

--- sDLM/routes/db.js
+++ sDLM/routes/db.js
@@ -2,54 +2,59 @@ var express = require('express');
 var mysql = require('mysql');
 var router = express.Router();
 var connection = mysql.createConnection({
-  host : '35.233.230.219',
-  user : 'root',
+  host: '35.233.230.219',
+  user: 'root',
   password: 'Love397!@',
   prot: 3306,
   database: 'test_trends'
 });
-connection.connect(function(err){
-  if(err) res.send(err);
+
+connection.connect(function (err) {
+  if (err) {
+    console.log(err);
+    res.send(err);
+  }
 });
 
 /* GET users listing. */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.send('DB RESTful API Page');
 });
 
-router.post('/', function(req, res){
+router.post('/', function (req, res) {
+
+
   var sql = "";
   console.log(req.body);
-  if(req.body.query_type == 'SELECT'){
-    sql += "SELECT "+req.body.column+" FROM "+req.body.table_name;
-    if(!req.body.where_value == ''){
-      sql += " WHERE "+req.body.where_value+";"
+  if (req.body.query_type == 'SELECT') {
+    sql += "SELECT " + req.body.column + " FROM " + req.body.table_name;
+    if (!req.body.where_value == '') {
+      sql += " WHERE " + req.body.where_value + ";"
     }
     console.log(sql);
-    connection.query(sql, function(err, rows){
-      if(!err) res.send(rows);
+    connection.query(sql, function (err, rows) {
+      if (!err) res.send(rows);
       else res.send(err);
     });
-  }else if(req.body.query_type == 'INSERT'){
-    sql += "INSERT INTO "+req.body.table_name+" "+req.body.column+" VALUES ?";
+  } else if (req.body.query_type == 'INSERT') {
+    sql += "INSERT INTO " + req.body.table_name + " " + req.body.column + " VALUES ?";
     var values = req.body.values;
-    connection.query(sql, [values], function(err, result){
-      if(!err){
+    connection.query(sql, [values], function (err, result) {
+      if (!err) {
         console.log(result);
         res.send(result);
-      } 
-      else {
+      } else {
         console.log(err);
         res.send(err);
       };
     });
-  }else if(req.body.query_type == 'DELETE'){
-    sql += "DELETE FROM "+req.body.table_name;
-    connection.query(sql, function(err, result){
-      if(!err) res.send(result);
+  } else if (req.body.query_type == 'DELETE') {
+    sql += "DELETE FROM " + req.body.table_name;
+    connection.query(sql, function (err, result) {
+      if (!err) res.send(result);
       else res.send(err);
     });
-  }else{
+  } else {
     console.log("QUERY ERROR!");
     res.send("Query Type Error");
   }
