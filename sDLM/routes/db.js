837223eb9b80699ce62824ba591278c22963ab5c
freight9 <kimseulki@freight9.com>
2018-11-09 16:35:20 +0900

    db connection 문제 수정

--- sDLM/routes/db.js
+++ sDLM/routes/db.js
@@ -1,18 +1,32 @@
 var express = require('express');
 var mysql = require('mysql');
 var router = express.Router();
-var connection = mysql.createConnection({
+var db_config = {
   host: '35.233.230.219',
   user: 'root',
   password: 'Love397!@',
   prot: 3306,
   database: 'db_trends'
-});
+}
+var connection = mysql.createConnection(db_config);
+
+function handleDisconnect() {
+  console.log('handleDisconnect()');
+  connection.destroy();
+  connection = mysql.createConnection(db_config);
+  connection.connect(function (err) {
+    if (err) {
+      console.log(' Error when connecting to db  (DBERR001):', err);
+      setTimeout(handleDisconnect, 1000);
+    }
+  });
+}
 
 connection.connect(function (err) {
   if (err) {
-    console.log(err);
-    res.send(err);
+    console.log('Connection is asleep (time to wake it up): ', err);
+    setTimeout(handleDisconnect, 1000);
+    handleDisconnect();
   }
 });
 
@@ -22,8 +36,6 @@ router.get('/', function (req, res) {
 });
 
 router.post('/', function (req, res) {
-
-
   var sql = "";
   console.log(req.body);
   if (req.body.query_type == 'SELECT') {
